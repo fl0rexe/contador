d96e3f18edf0ef5cad74f09a735241355243871e
EX3QU1EL <89056349+fl0rexe@users.noreply.github.com>
2021-09-18 04:26:00 -0300

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2021, // required
 	month: 9, // required
 	day: 18, // required
-	hours: 7, // Default is 0 [0-23] integer
+	hours: 0, // Default is 0 [0-23] integer
 	minutes: 18, // Default is 0 [0-59] integer
 	seconds: 20, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
